27ca61b3379f3fd51495198bdab26b8c2ddb5d2e
Izzuddin Natsir <32354148+izznatsir@users.noreply.github.com>
2023-11-26 14:59:17 +0700

    wip cli colors command

---
+++ packages/nizedash/src/bin.ts
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+import { generateColors } from "./commands/colors.js";
+
+function main() {
+	let [command, ...args] = process.argv.slice(2);
+
+	switch (command) {
+		case "colors": {
+			generateColors();
+			break;
+		}
+	}
+}
+
+try {
+	main();
+} catch (error) {
+	console.error(error);
+	process.exit(1);
+}
